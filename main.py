d0cfe6ae7f4fa38de5f766b9030c03d392a819be
Sahil-pixel <57060638+Sahil-pixel@users.noreply.github.com>
2021-02-07 23:47:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.core.audio import SoundLoader
+
+class A(App):
+	def build(self):
+		self.sound = SoundLoader.load('app.mp3')
+		
+		return Button(text='play',on_release=self.splay)
+	def splay(self,obj):
+		if self.sound:
+			print("Sound found at %s" % self.sound.source)
+			print("Sound is %.3f seconds" % self.sound.length)
+			self.sound.play()
+
+A().run()
